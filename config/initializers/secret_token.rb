e860bfc03a7f928249127d384f1aa6c6618365d0
Tajima Junpei <p.baleine@gmail.com>
2013-06-11 08:00:35 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBackboneNestedModel::Application.config.secret_token = 'd83c33f3bb735187d5b5edb29dbc4cbeb2f8e357f334fa96c4082090639c78623e34c54338066dfd1eef3831b091e1284c622031eae180a4b72b019272c65f72'
